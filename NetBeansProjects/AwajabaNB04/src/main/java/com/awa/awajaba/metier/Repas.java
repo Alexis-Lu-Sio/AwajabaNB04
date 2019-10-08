51a3b3a1578a43e0380db4e9929eeac6dc464df2
Lu <alexis.lu2.sio@gmail.com>
2019-10-08 16:19:31 +0200

    commit #3

--- NetBeansProjects/AwajabaNB04/src/main/java/com/awa/awajaba/metier/Repas.java
+++ NetBeansProjects/AwajabaNB04/src/main/java/com/awa/awajaba/metier/Repas.java
@@ -14,4 +14,10 @@ import java.time.LocalDate;
 public class Repas {
     public int numero;
     public LocalDate date;
+    
+    Repas(int numero, LocalDate date){
+        this.numero = numero;
+        this.date = date;
+	
+    }
 }
